9ff6b5bb5d5ea1926157bdfe5dc0010bce0afdc9
Jeffrey Olchovy <jeffo@tapad.com>
2017-05-29 10:38:28 +0200

    regression test for https support for MarathonService URL - addresses #2

---
+++ marathon/src/test/scala/sbtmarathon/MarathonServiceFunctionalSpec.scala
@@ -0,0 +1,54 @@
+package sbtmarathon
+
+import java.net.URL
+import com.twitter.finagle.http.{RequestBuilder, Request}
+import org.scalatest.{FlatSpec, Matchers, EitherValues}
+
+class MarathonServiceFunctionalSpec extends FlatSpec with Matchers with EitherValues {
+
+  behavior of "MarathonService"
+
+  it should "execute requests against HTTP URLs with no explicit port specified" in {
+    val url = new URL("http://example.com")
+    val service = new MarathonService(url)
+    val request = getRequest(url)
+    val result = service.executeRequest(request, url)
+    result.isGood shouldBe true
+    val resultE = result.toEither
+    resultE.right.value shouldBe a [MarathonService.Success]
+  }
+
+  it should "execute requests against HTTP URLs with an explicit port specified" in {
+    val url = new URL("http://example.com:80")
+    val service = new MarathonService(url)
+    val request = getRequest(url)
+    val result = service.executeRequest(request, url)
+    result.isGood shouldBe true
+    val resultE = result.toEither
+    resultE.right.value shouldBe a [MarathonService.Success]
+  }
+
+  it should "execute requests against HTTPs URLs with no explicit port specified" in {
+    val url = new URL("https://example.com")
+    val service = new MarathonService(url)
+    val request = getRequest(url)
+    val result = service.executeRequest(request, url)
+    result.isGood shouldBe true
+    val resultE = result.toEither
+    resultE.right.value shouldBe a [MarathonService.Success]
+  }
+
+  it should "execute requests against HTTPs URLs with an explicit port specified" in {
+    val url = new URL("https://example.com:443")
+    val service = new MarathonService(url)
+    val request = getRequest(url)
+    val result = service.executeRequest(request, url)
+    result.isGood shouldBe true
+    val resultE = result.toEither
+    resultE.right.value shouldBe a [MarathonService.Success]
+  }
+
+  def getRequest(url: URL): Request = {
+    RequestBuilder().url(url).buildGet()
+  }
+}
