fbab208adb7227664fc4147c746b1eed81a2d3a1
Jeffrey Olchovy <jeffo@tapad.com>
2017-05-30 16:22:18 +0200

    ensure regression test executes a request against an application instance URL

--- marathon/src/test/scala/sbtmarathon/MockAuthenticatingMarathonServerSpec.scala
+++ marathon/src/test/scala/sbtmarathon/MockAuthenticatingMarathonServerSpec.scala
@@ -13,10 +13,15 @@ class MockAuthenticatingMarathonServerSpec extends FlatSpec with Matchers with E
     withServer("user" -> "password") { server =>
       val url = new URL(s"http://user:password@127.0.0.1:${server.port}")
       val service = new MarathonService(url)
-      val result = service.start(s"""{"id":"foo"}""")
-      result.isGood shouldBe true
-      val resultE = result.toEither
-      resultE.right.value shouldBe a [MarathonService.Success]
+      val applicationId = "foo"
+      val result1 = service.start(s"""{"id":"$applicationId"}""")
+      result1.isGood shouldBe true
+      val resultE1 = result1.toEither
+      resultE1.right.value shouldBe a [MarathonService.Success]
+      val result2 = service.destroy(applicationId)
+      result2.isGood shouldBe true
+      val resultE2 = result2.toEither
+      resultE2.right.value shouldBe a [MarathonService.Success]
     }
   }
 
