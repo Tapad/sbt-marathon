157e4e9474be6f676ebf545adb9d344c35a6100a
Jeffrey Olchovy <olchovy@gmail.com>
2017-10-26 22:34:10 +0200

    add a logging impl to templating configuration, toggleable

--- templating/src/main/scala/sbtmarathon/TemplatingKeys.scala
+++ templating/src/main/scala/sbtmarathon/TemplatingKeys.scala
@@ -5,4 +5,5 @@ import sbt._
 object TemplatingKeys {
   val marathonTemplates = taskKey[Seq[Template]]("Template instances")
   val marathonEvaluateTemplates = taskKey[Seq[File]]("Evaluate templates")
+  val marathonTemplateLoggingEnabled = settingKey[Boolean]("Include a logging implementation in the Templating ivy configuration")
 }
